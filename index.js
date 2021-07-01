164db6c347c245593e85b4a1d1e8cada7b58ed2c
willianbrac <66275588+willianbrac@users.noreply.github.com>
2021-06-30 20:23:21 -0500

    Adicionando o HATEOAS

--- index.js
+++ index.js
@@ -66,8 +66,31 @@ var DB = {
 }
 
 app.get("/games", auth, (req, res) => {
+    var HATEOAS = [
+        {
+            href: "http://localhost:9090/game/0",
+            method: "DELETE",
+            rel: "delete_game"
+        },
+        {
+            href: "http://localhost:9090/game/0",
+            method: "GET",
+            rel: "get_game"
+        },
+        {
+            href: "http://localhost:9090/game/0",
+            method: "PUT",
+            rel: "edit_game"
+        },
+        {
+            href: "http://localhost:9090/auth",
+            method: "POST",
+            rel: "login"
+        }
+    ]
+
     res.statusCode = 200;
-    res.json(DB.games);
+    res.json({games:DB.games, _links: HATEOAS});
 });
 
 app.get("/game/:id", auth, (req, res) => {
@@ -76,6 +99,28 @@ app.get("/game/:id", auth, (req, res) => {
     }else{
         
         var id = parseInt(req.params.id);
+        var HATEOAS = [
+            {
+                href: "http://localhost:9090/game/"+id,
+                method: "DELETE",
+                rel: "delete_game"
+            },
+            {
+                href: "http://localhost:9090/game/"+id,
+                method: "GET",
+                rel: "get_game"
+            },
+            {
+                href: "http://localhost:9090/game/"+id,
+                method: "PUT",
+                rel: "edit_game"
+            },
+            {
+                href: "http://localhost:9090/games",
+                method: "get",
+                rel: "get_all_games"
+            }
+        ]
 
         var game = DB.games.find(g => g.id == id);
 
@@ -83,7 +128,7 @@ app.get("/game/:id", auth, (req, res) => {
             res.sendStatus(404);
         }else{
             res.statusCode = 200;
-            res.json(game);
+            res.json({game: game, _links: HATEOAS});
         }
     }
 });
